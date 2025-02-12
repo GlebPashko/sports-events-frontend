3a66178afa7fd58250a6ba24c18295b462baccd9
GlebPashko <glebpashko2004@gmail.com>
2025-02-12 11:07:37 +0200

    Improved menu component

--- src/components/menu/components/Menu.js
+++ src/components/menu/components/Menu.js
@@ -4,8 +4,7 @@ import "../styles/style.scss";
 
 const Menu = () => {
     return (
-        <section className="menuSection">
-            <div className="menu">
+        <section className="menu">
                 <nav>
                     <ul className="menu__list">
                         <li><Link className="menu__link" to="/">Головна</Link></li>
@@ -13,7 +12,6 @@ const Menu = () => {
                         <li><Link className="menu__link" to="/login">Авторизація</Link></li>
                     </ul>
                 </nav>
-            </div>
         </section>
     );
 };
